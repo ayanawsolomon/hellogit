c6d8012417d2f8cc877a36609b28752ca73a6841
solomon <ayanawsolomon1@gmail.com>
2017-07-08 00:18:46 -0400

    added hello solomon print message line 9

--- HelloGit/src/net/solomon/hellogit/HellogGit.java
+++ HelloGit/src/net/solomon/hellogit/HellogGit.java
@@ -4,6 +4,8 @@ public class HellogGit {
 
 	public static void main(String[] args) {
 		//a greeting
+		// addming more comment
+		System.out.println("Hello solomon");
 		System.out.println("Hello Git");
 	}
 
