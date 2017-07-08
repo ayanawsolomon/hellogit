4901ab35dd605a866e7a630dc6a752f8f5268d75
solomon <ayanawsolomon1@gmail.com>
2017-07-08 01:17:07 -0400

    Developer 2 added dev2 on line 9

--- HelloGit/src/net/solomon/hellogit/HellogGit.java
+++ HelloGit/src/net/solomon/hellogit/HellogGit.java
@@ -6,7 +6,7 @@ public class HellogGit {
 		//a greeting
 		// addming more comment
 		System.out.println("Hello solomon");
-		System.out.println("Hello Git");
+		System.out.println("Hello dev2");
 		// I am adding this comment from git hub site
 		// more comment from git hub agina
 	System.out.println("this is from local");
