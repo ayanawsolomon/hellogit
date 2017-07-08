4f7981ed68ac6ddf156bbcfa64e36c19c99c9043
solomon <ayanawsolomon1@gmail.com>
2017-07-08 19:22:49 -0400

    Showing raju, adding a comment on line 8

--- HelloGit/src/net/solomon/hellogit/HellogGit.java
+++ HelloGit/src/net/solomon/hellogit/HellogGit.java
@@ -5,6 +5,7 @@ public class HellogGit {
 	public static void main(String[] args) {
 		//a greeting
 		//2222222222222221111111111111114444445555555
+		// this is for raju
 		// addming more comment
 		System.out.println("Hello solomon");
 		System.out.println("Hello Git Deve 1 added this too hello deve2 Hello dev2 dev1cvccccc");
