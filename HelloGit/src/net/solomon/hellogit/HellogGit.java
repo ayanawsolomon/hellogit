909a7d53c7adec3864a8f648cc7b278760fe494b
ayanawsolomon <ayanawsolomon@yahoo.com>
2017-07-08 00:39:12 -0400

    yes  adding 2nd comment
    more description about 2nd comment

--- HelloGit/src/net/solomon/hellogit/HellogGit.java
+++ HelloGit/src/net/solomon/hellogit/HellogGit.java
@@ -8,6 +8,7 @@ public class HellogGit {
 		System.out.println("Hello solomon");
 		System.out.println("Hello Git");
 		// I am adding this comment from git hub site
+		// more comment from git hub agina
 	}
 
 }
