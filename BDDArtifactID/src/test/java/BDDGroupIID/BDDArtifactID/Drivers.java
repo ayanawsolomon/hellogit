073b24f3636b63f8b8497501da712d6e26e42f35
solomon <ayanawsolomon1@gmail.com>
2017-07-15 14:00:09 -0400

    Driver java has modefied

--- BDDArtifactID/src/test/java/BDDGroupIID/BDDArtifactID/Drivers.java
+++ BDDArtifactID/src/test/java/BDDGroupIID/BDDArtifactID/Drivers.java
@@ -8,22 +8,5 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import cucumber.api.java.Before;
 
 public class Drivers {
-	// singlton method to instantiate the driver just one time only
-    private static boolean initialized = false;
-    private static WebDriver driver;
-
-    @Before
-    public void initialize(){
-        if (!initialized){
-            initialized = true;
-            System.setProperty("webdriver.chrome.driver", "C:\\Users\\solomon\\workspace\\MyDrivers\\chromedriver.exe");
-    		driver = new ChromeDriver();
-    		driver.get("http://newtours.demoaut.com/mercuryregister.php");
-    		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);	// wait will be applied for any element not available for the first try
-    		
-        }
-    }
-    public static WebDriver getDriver(){
-        return driver;
-    }   
+	
 }
