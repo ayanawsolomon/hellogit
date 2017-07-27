f092403fa28528b578409c792809f8219c25ea9c
solomon <ayanawsolomon1@gmail.com>
2017-07-27 00:21:50 -0400

    test

--- HelloGit/src/net/solomon/hellogit/HellogGit.java
+++ HelloGit/src/net/solomon/hellogit/HellogGit.java
@@ -8,7 +8,7 @@ public class HellogGit {
 		//2222222222222221111111111111114444445555555
 		// this is for raju
 		// addming more comment
-		System.out.println("Hello solomon");
+		System.out.println("Hello solomon 11111111111111111");
 		System.out.println("Hello Git Deve 1 added this too hello deve2 Hello dev2 dev1cvccccc");
 		
 
